05bab83ee21f20ab98222f4bd230580c50d7dac9
IFang Lee <IFang.Lee@siriusxm.com>
2024-02-22 19:22:54 -0800

    navigation Style animation

--- animation/animation/ContentView.swift
+++ animation/animation/ContentView.swift
@@ -10,30 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var showView: Bool = false
     var body: some View {
-        VStack {
-            SourceView(id: "View 1") {
-                Circle()
-                    .fill(.red)
-                    .frame(width: 50, height: 50)
-                    .onTapGesture {
-                        showView.toggle()
-                    }
+        NavigationStack {
+            VStack {
+                SourceView(id: "View 1") {
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 50, height: 50)
+                        .onTapGesture {
+                            showView.toggle()
+                        }
+                }
+            }
+            .padding()
+            .navigationTitle("Navigation Style")
+            .navigationDestination(isPresented: $showView) {
+                DestinationView(id: "View 1") {
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 150, height: 150)
+                        .onTapGesture {
+                            showView.toggle()
+                        }
+                }
+                .padding(15)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                .navigationBarBackButtonHidden() // animation effect requires to disable interaction such as go back
+                .navigationTitle("Detail View")
             }
         }
-        .padding()
-        .fullScreenCover(isPresented: $showView, content: {
-            DestinationView(id: "View 1") {
-                Circle()
-                    .fill(.red)
-                    .frame(width: 150, height: 150)
-                    .onTapGesture {
-                        showView.toggle()
-                    }
-            }
-            .padding(15)
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
-            .interactiveDismissDisabled() // animation effect requires to disable interaction such as go back or dismiss sheet
-        })
         .heroLayer(id: "View 1", animate: $showView) {
             Circle()
                 .fill(.red)
