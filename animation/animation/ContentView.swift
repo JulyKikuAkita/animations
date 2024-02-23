918bb79ab8adf8eb4fd5cb9a532a484196c82c60
IFang Lee <IFang.Lee@siriusxm.com>
2024-02-22 17:57:37 -0800

    wip

---
+++ animation/animation/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  animation
+//
+//  Created by IFang Lee on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showView: Bool = false
+    var body: some View {
+        VStack {
+            SourceView(id: "View 1") {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 50, height: 50)
+                    .onTapGesture {
+                        showView.toggle()
+                    }
+            }
+        }
+        .padding()
+        .sheet(isPresented: $showView, content: {
+            DestinationView(id: "View 1") {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 150, height: 150)
+                    .onTapGesture {
+                        showView.toggle()
+                    }
+            }
+            .padding(150)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+            .interactiveDismissDisabled() // animation effect requires to disable interaction such as go back or dismiss sheet
+        })
+        .heroLayer(id: "View 1", animate: $showView) {
+            Circle()
+                .fill(.red)
+        } completion: { status in
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ animation/animation/animationApp.swift
@@ -0,0 +1,19 @@
+//
+//  animationApp.swift
+//  animation
+//
+//  Created by IFang Lee on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HeroWrapper {
+                ContentView()
+            }
+        }
+    }
+}
