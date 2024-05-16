929565f2862fff2326f5b773cb60105ffc3ad6c9
IFang Lee <b92701105@gmail.com>
2024-05-15 20:38:58 -0700

    adding search textfield

--- MyMint/View/TabViews/Search.swift
+++ MyMint/View/TabViews/Search.swift
@@ -1,15 +1,38 @@
 //
 //  Search.swift
 //  MyMint
-//
-//  Created by IFang Lee on 5/14/24.
-//
 
 import SwiftUI
-
+import Combine
 struct Search: View {
+    /// View Properties
+    @State private var searchText: String = ""
+    @State private var filterText: String = ""
+    let searchPublisher = PassthroughSubject<String, Never>() // use combine debounce method
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 12) {
+                    
+                }
+            }
+            .overlay(content: {
+                ContentUnavailableView("Search Transactions", image: "magnifyingglass")
+                    .opacity(filterText.isEmpty ? 1 : 0)
+            })
+            .onChange(of: searchText, { oldValue, newValue in
+                if newValue.isEmpty {
+                    filterText = ""
+                }
+                searchPublisher.send(newValue)
+            })
+            .onReceive(searchPublisher.debounce(for: .seconds(0.3), scheduler: DispatchQueue.main), perform: { text in
+                filterText = text
+            })
+            .searchable(text: $searchText)
+            .navigationTitle("Search")
+            .background(.gray.opacity(0.15))
+        }
     }
 }
 
