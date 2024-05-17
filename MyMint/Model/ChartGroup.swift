1f54fc66c67217383f9de627d687291f0b7d051f
IFang Lee <b92701105@gmail.com>
2024-05-17 00:25:23 -0700

    adding chartgroup model

---
+++ MyMint/Model/ChartGroup.swift
@@ -0,0 +1,28 @@
+//
+//  ChartGroup.swift
+//  MyMint
+
+import SwiftUI
+
+struct ChartGroup: Identifiable {
+    let id: UUID = .init()
+    var date:Date
+    var categories: [MintCategory]
+    var totalIncome: Double
+    var totalExpense: Double
+}
+
+struct ChartCategory: Identifiable {
+    let id: UUID = .init()
+    var totalValue: Double
+    var category: MintCategory
+}
+
+struct ChartRulesGroup: Identifiable {
+    let id: UUID = .init()
+    var date:Date
+    var rule: [MintRule]
+    var totalNeed: Double
+    var totalWant: Double
+    var totalSave: Double
+}
--- MyMint/View/TabViews/Graphs.swift
+++ MyMint/View/TabViews/Graphs.swift
@@ -1,13 +1,17 @@
 //
 //  Graphs.swift
 //  MyMint
-//
-//  Created by IFang Lee on 5/14/24.
-//
 
 import SwiftUI
+import SwiftData
+import Charts
 
+// TODO: https://www.youtube.com/watch?v=qQ3NGkv8O8c&list=PLimqJDzPI-H88PbxlOtNPkD0n0n-q-__z&index=7
+// 2:00
 struct Graphs: View {
+    @Query(animation: .snappy) private var transactions: [Transaction]
+    @State private var chartGroups: [ChartGroup] = []
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
