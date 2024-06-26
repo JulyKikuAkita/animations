d2d626b2c2e058b4f137fe661dce73b9b4d7caab
IFang Lee <b92701105@gmail.com>
2024-06-25 23:32:35 -0700

    add text selection api demo

--- View/TextFieldView/TextFieldSelectionAPIView.swift
+++ View/TextFieldView/TextFieldSelectionAPIView.swift
@@ -19,6 +19,50 @@ struct TextFieldSelectionAPIView: View {
                     .frame(height: 150)
                     .background(.background, in: .rect(cornerRadius: 10))
                 
+                VStack(spacing: 15) {
+                    HStack(spacing: 20) {
+                        Button("Move After Hello World") {
+                            if let range = text.range(of: "Hello") {
+                                let endIndex = range.upperBound
+                                selection = .init(insertionPoint: endIndex)
+                            }
+                        }
+                        
+                        Button("Select Hello World") {
+                            if let range = text.range(of: "Hello") {
+                                selection = .init(range: range)
+                            }
+                        }
+                    }
+                    
+                    HStack(spacing: 20) {
+                        Button("Move to First") {
+                            selection = .init(insertionPoint: text.startIndex)
+                        }
+                        
+                        Button("Move to Last") {
+                            selection = .init(insertionPoint: text.endIndex)
+                        }
+                    }
+                    
+                    if let selectedTextRange, text[selectedTextRange] == "Hello" {
+                        Button("Replace with Hi There") {
+                            text.replaceSubrange(selectedTextRange, with: "Hi There")
+                            // update selection text
+                            let startIndex = selectedTextRange.lowerBound
+                            let length = "Hi There".count
+                            let endIndex = text.index(
+                                startIndex,
+                                offsetBy: length
+                            )
+                            let newRange: Range<String.Index> = .init(
+                                uncheckedBounds: (startIndex, endIndex)
+                            )
+                            selection = .init(range: newRange)
+                        }
+                    }
+                }
+                
                 Spacer(minLength: 0)
             }
             .padding(15)
@@ -37,6 +81,18 @@ struct TextFieldSelectionAPIView: View {
                 }
             }
         }
+    }
+    
+    var selectedTextRange: Range<String.Index>? {
+        if let selection, !selection.isInsertion {
+            switch selection.indices {
+            case .selection(let range):
+                return range
+            default: return nil
+            }
+        }
+        
+        return nil
     }
 }
 
