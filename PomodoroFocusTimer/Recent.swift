d540d75918ac553005dd8215d2217164afc9518c
IFang Lee <b92701105@gmail.com>
2024-06-28 23:21:46 -0700

    add pomodoro timer demo app

---
+++ PomodoroFocusTimer/Recent.swift
@@ -0,0 +1,22 @@
+//
+//  Recent.swift
+//  demoApp
+//
+//  Swift Data Model
+//
+
+import SwiftUI
+import SwiftData
+
+@Model
+class Recent {
+    var hours: Int
+    var minutes: Int
+    var seconds: Int
+    var date: Date = Date() /// for sorting list
+    init(hours: Int, minutes: Int, seconds: Int) {
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+    }
+}
