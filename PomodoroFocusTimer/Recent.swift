e5098440472090ba1f5eb248804f15cccd489246
IFang Lee <b92701105@gmail.com>
2024-06-30 16:06:04 -0700

    update picker clock and flipclock, finish timer view

--- PomodoroFocusTimer/Recent.swift
+++ PomodoroFocusTimer/Recent.swift
@@ -14,9 +14,14 @@ class Recent {
     var minutes: Int
     var seconds: Int
     var date: Date = Date() /// for sorting list
+    ///
     init(hours: Int, minutes: Int, seconds: Int) {
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
+    }
+    
+    var totalInSeconds: Int {
+        return (hours * 60 * 60) + (minutes * 60) + seconds
     }
 }
