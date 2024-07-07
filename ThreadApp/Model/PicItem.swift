e4765e9dbe98e18fa342f7281272437d46b64f4f
IFang Lee <b92701105@gmail.com>
2024-07-07 16:03:58 -0700

    adding DetailView and model

---
+++ ThreadApp/Model/PicItem.swift
@@ -0,0 +1,32 @@
+//
+//  PicItem.swift
+//  ThreadApp
+
+import SwiftUI
+
+// Model
+
+struct PicItem: Identifiable {
+    let id: UUID = .init()
+    var image: String
+}
+/// Constructing pic using asset images
+private var pics: [PicItem] = (1...5).compactMap{ index -> PicItem? in
+    return .init(image: "IMG_020\(index + 1)")
+}
+
+
+struct Post: Identifiable{
+    let id: UUID = .init()
+    var username: String
+    var content: String
+    var pics: [PicItem]
+    /// View based properties
+    var scrollPosition: UUID?
+}
+
+/// Sample Posts
+var samplePosts: [Post] = [
+    .init(username: "Nanachi", content: "Natural Pics", pics: pics),
+    .init(username: "Nanachi", content: "Natural Pics", pics: pics.reversed())
+]
--- ThreadApp/SynchronizedScrollView.swift
+++ ThreadApp/SynchronizedScrollView.swift
@@ -6,23 +6,46 @@
 import SwiftUI
 
 struct SynchronizedScrollView: View {
-    @State private var pics: [PicItem] = (1...5).compactMap{ index -> PicItem? in
-        return .init(image: "IMG_020\(index + 1)")
-    }
+    /// View Properties
+    @State private var posts: [Post] = samplePosts
+    @State private var showDetailView: Bool = false
+    @State private var selectedPicID: UUID?
+    @State private var selectedPost: Post?
+    
     var body: some View {
         NavigationStack {
             ScrollView(.vertical) {
                 VStack(spacing: 15) {
-                    CardView(pics)
+                    ForEach(posts) { post in
+                        CardView(post)
+
+                    }
                 }
                 .safeAreaPadding(15)
             }
             .navigationTitle("PICS")
         }
+        .overlay {
+            if let selectedPost, showDetailView {
+                DetailView(
+                    post: selectedPost,
+                    showDetailView: $showDetailView,
+                    selectedPicID: $selectedPicID
+                ) { id in
+                    /// Updating scroll position
+                    if let index = posts.firstIndex(
+                        where: { $0.id == selectedPost
+                            .id}) {
+                        posts[index].scrollPosition = id
+                    }
+                }
+                .transition(.offset(y: 5)) /// try use identity transition to see the diff
+            }
+        }
     }
     
     @ViewBuilder
-    func CardView(_ pics: [PicItem]) -> some View {
+    func CardView(_ post: Post) -> some View {
         VStack(spacing: 10) {
             HStack(spacing: 10) {
                 Image(systemName: "person.circle.fill")
@@ -32,11 +55,11 @@ struct SynchronizedScrollView: View {
                     .background(.background)
                 
                 VStack(alignment: .leading, spacing: 4, content: {
-                    Text("Nanachi")
+                    Text(post.username)
                         .fontWeight(.semibold)
                         .textScale(.secondary)
                     
-                    Text("Nature Pics")
+                    Text(post.content)
                 })
                 
                 Spacer(minLength: 0)
@@ -53,17 +76,30 @@ struct SynchronizedScrollView: View {
                     let size = $0.size
                     
                     ScrollView(.horizontal) {
-                        LazyHStack(spacing: 10) {
-                            ForEach(pics) { pic in
-                                Image(pic.image)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(maxWidth: size.width)
-                                    .clipShape(.rect(cornerRadius: 10))
+                        HStack(spacing: 10) {
+                            ForEach(post.pics) { pic in
+                                LazyHStack {
+                                    Image(pic.image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(maxWidth: size.width)
+                                        .clipShape(.rect(cornerRadius: 10))
+                                }
+                                .frame(maxWidth: size.width)
+                                .frame(height: size.height)
+                                .onTapGesture {
+                                    selectedPost = post
+                                    selectedPicID = pic.id
+                                    showDetailView = true
+                                }
+                                .contentShape(.rect)
                             }
                         }
                         .scrollTargetLayout()
                     }
+                    .scrollPosition(id: .init(get: {
+                        return post.scrollPosition
+                    }, set: { _ in }))
                     .scrollIndicators(.hidden)
                     .scrollTargetBehavior(.viewAligned)
                     .scrollClipDisabled()
@@ -88,7 +124,7 @@ struct SynchronizedScrollView: View {
                     }
                 }
             }
-            .padding(.leading, 45)
+            .safeAreaPadding(.leading, 45)
             
             
             /// Likes and replies
@@ -135,7 +171,46 @@ struct SynchronizedScrollView: View {
     SynchronizedScrollView()
 }
 
-struct PicItem: Identifiable, Hashable {
-    let id: UUID = .init()
-    var image: String
+private struct DetailView: View {
+    var post: Post
+    @Binding var showDetailView: Bool
+    @Binding var selectedPicID: UUID?
+    var updateScrollPosition: (UUID?) -> ()
+    /// View Properties
+    @State private var detailScrollPosition: UUID?
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 0) {
+                ForEach(post.pics) { pic in
+                    Image(pic.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .containerRelativeFrame(.horizontal)
+                        .clipped()
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollPosition(id: $detailScrollPosition)
+        .background(.black)
+        .scrollTargetBehavior(.paging)
+        .scrollIndicators(.hidden)
+        /// close button
+        .overlay(alignment: .topLeading) {
+            Button("", systemImage: "xmark.circle.fill") {
+                updateScrollPosition(detailScrollPosition)
+                showDetailView = false
+                selectedPicID = nil
+            }
+            .font(.title)
+            .foregroundStyle(.white.opacity(0.8), .white.opacity(0.15))
+            .padding()
+        }
+        .onAppear {
+            /// avoid multiple calls
+            guard detailScrollPosition == nil else { return }
+            detailScrollPosition = selectedPicID /// make sure carousel start from the select image
+        }
+        
+    }
 }
