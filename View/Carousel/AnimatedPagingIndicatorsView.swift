0e59ed9323240459140146d3c96616554842e061
IFang Lee <b92701105@gmail.com>
2024-07-15 22:56:34 -0700

    adding Animated Paging Indicators - iOS 17 APIs

---
+++ View/Carousel/AnimatedPagingIndicatorsView.swift
@@ -0,0 +1,70 @@
+//
+//  AnimatedPagingIndicatorsView.swift
+//  animation
+
+import SwiftUI
+
+struct PagingIndicatorsDemoView: View {
+    var body: some View {
+        NavigationStack {
+            AnimatedPagingIndicatorsView()
+        }
+    }
+}
+
+// 1:51
+struct AnimatedPagingIndicatorsView: View {
+    /// View Properties
+    @State private var colors: [Color] = [.pink, .purple, .blue, .green]
+    @State private var opacityEffect: Bool = false
+    @State private var clipEdges: Bool = false
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(colors, id: \.self) { color in
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(color.gradient)
+                            .padding(.horizontal, 5)
+                            .containerRelativeFrame(.horizontal)
+                    }
+                }
+                .overlay(alignment: .bottom) {
+                    PageIndicatorView(
+                        activeTint: .white,
+                        inactiveTint: .black.opacity(0.25),
+                        opacityEffect: opacityEffect,
+                        clipEdges: clipEdges
+                    )
+                }
+            }
+            .scrollTargetBehavior(.paging) // comment this out for carousel style
+            .scrollIndicators(.hidden)
+            .frame(height: 220)
+            .padding(15)  // comment this out for carousel style
+            /// use below for carousel style
+//            .scrollTargetBehavior(.viewAligned)
+//            .safeAreaPadding(.vertical, 15)
+//            .safeAreaPadding(.horizontal, 25)
+
+            List {
+                Section("Options") {
+                    Toggle("Opacity Effect", isOn: $opacityEffect)
+                    Toggle("Clip Edges", isOn: $clipEdges)
+                    
+                    Button("Add Card") {
+                        colors.append(.brown)
+                    }
+
+                }
+            }
+            .clipShape(.rect(cornerRadius: 15))
+            .padding(15)
+        }
+        .navigationTitle("Custom Indicator")
+    }
+}
+
+#Preview {
+    PagingIndicatorsDemoView()
+}
---
+++ View/HelpView/PageIndicatorView.swift
@@ -0,0 +1,70 @@
+//
+//  PageIndicatorView.swift
+//  animation
+import SwiftUI
+
+
+struct PageIndicatorView: View {
+    var activeTint: Color = .primary
+    var inactiveTint: Color = .primary.opacity(0.15)
+    var opacityEffect: Bool = false
+    var clipEdges: Bool = false
+    
+    var body: some View {
+        GeometryReader {
+            /// Entire view size for calculating page indicators
+            let width = $0.size.width
+            /// ScrollView bounds
+            if let scrollViewWidth = $0.bounds(
+                of: .scrollView(axis: .horizontal)
+            )?.width, scrollViewWidth > 0 {
+                let minX = $0.frame(in: .scrollView(axis: .horizontal)).minX
+                let totalPages = Int(width / scrollViewWidth)
+                /// Progress
+                let freeProgress = -minX / scrollViewWidth
+                let clippedProgress = min(
+                    max(freeProgress, 0.0),
+                    CGFloat(totalPages - 1)
+                )
+                let progress = clipEdges ? clippedProgress : freeProgress
+                /// Index
+                let activeIndex = Int(progress)
+                let nextIndex = Int(progress.rounded(.awayFromZero))
+                
+                let indicatorProgress = progress - CGFloat(activeIndex)
+                /// Indicator width: current and upcoming
+                /// 18 = indicator width of 8 and the hstack spacing of 10
+                let currentPageWidth = 18 - (indicatorProgress * 18)
+                let nextPageWidth = indicatorProgress * 18
+
+                HStack(spacing: 10) {
+                    ForEach(0..<totalPages, id: \.self) { index in
+                        Capsule()
+                            .fill(.clear)
+                            .frame(
+                                width: 8 + (
+                                    activeIndex == index ? currentPageWidth
+                                    : nextIndex == index ? nextPageWidth : 0),
+                                height: 8
+                            )
+                            .overlay {
+                                ZStack {
+                                    Capsule()
+                                        .fill(inactiveTint)
+                                    
+                                    Capsule()
+                                        .fill(activeTint)
+                                        .opacity(opacityEffect ? activeIndex == index ? 1 -  indicatorProgress
+                                                 : nextIndex == index ? indicatorProgress : 0 : 1)
+                                    
+                                }
+                            }
+                    }
+                }
+                .frame(width: scrollViewWidth)
+                .offset(x: -minX)
+            }
+        }
+        .frame(height: 30)
+    }
+}
