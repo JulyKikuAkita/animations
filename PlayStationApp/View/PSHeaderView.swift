f5ce6016e3b4b2d9904a1b72fefbe27e0ba5f4dd
IFang Lee <b92701105@gmail.com>
2024-08-25 23:24:51 -0700

    adding custom PSHeaderView

--- PlayStationApp/View/PSHeaderView.swift
+++ PlayStationApp/View/PSHeaderView.swift
@@ -9,19 +9,40 @@ struct PSHeaderView: View {
     /// View Properties
     @State private var activeTab: PSHeaderTab = .chat
     var body: some View {
-        if #available(iOS 18, *) {
-//            TabView(selection: $activeTab) {
-//                SwiftUI.Tab.init(value: .chat) {
-//                    Text("Chat")
-//                }
-//                
-//                SwiftUI.Tab.init(value: .friends) {
-//                    Text("Friends")
-//                }
-//            }
-        } else {
+        let height: CGFloat = size.height + safeArea.top
+        
+        VStack(spacing: 0) {
+            Group {
+                if #available(iOS 18, *) {
+                    TabView(selection: $activeTab) {
+                        SwiftUI.Tab.init(value: .chat) {
+                            Text("Chat")
+                        }
+                        
+                        SwiftUI.Tab.init(value: .friends) {
+                            Text("Friends")
+                        }
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                } else {
+                    TabView(selection: $activeTab) {
+                        Text("Chat")
+                            .tag(PSHeaderTab.chat)
+                        
+                        Text("Friends")
+                            .tag(PSHeaderTab.friends)
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
+                }
+            }
             
+            /// minimized header view
+            Rectangle()
+                .fill(.pink)
+                .frame(height: 50)
         }
+        .frame(height: height)
+        .offset(y: -(height - 50))
     }
 }
 
