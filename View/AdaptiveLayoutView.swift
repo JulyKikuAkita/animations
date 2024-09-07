34449514b775e685c086500cc815e391e216fe66
IFang Lee <b92701105@gmail.com>
2024-09-07 00:21:46 -0700

    SwiftUI Responsive UI Design - Adaptive Layout - iOS 18

--- View/AdaptiveLayoutView.swift
+++ View/AdaptiveLayoutView.swift
@@ -23,120 +23,148 @@ enum TabState: String, CaseIterable {
 struct AdaptiveLayoutView: View {
     /// View Properties
     @State private var activeTab: TabState = .home
+    @State private var panGesture: UIPanGestureRecognizer?
     /// Gesture Properties
     @State private var offset: CGFloat = .zero
     @State private var lastDragOffset: CGFloat = .zero
     @State private var progress: CGFloat = .zero
-
+    /// Navigation Path
+    @State private var navigationPath: NavigationPath = .init()
+    
     var body: some View {
-        
-        GeometryReader {
-            let size = $0.size
-            let sideBarWidth: CGFloat = 250
+        AdaptiveView { size, isLandscape in
+            let sideBarWidth: CGFloat = isLandscape ? 220 : 250
+            let layout = isLandscape ? AnyLayout(
+                HStackLayout(spacing: 0)
+            ) : AnyLayout(ZStackLayout(alignment: .leading))
             
-            ZStack(alignment: .leading) {
-                SideBarView()
+            NavigationStack(path: $navigationPath) {
+                layout {
+                    SideBarView(path: $navigationPath) {
+                        toggleSideBar() 
+                    }
                     .frame(width: sideBarWidth)
-                    .offset(x: -sideBarWidth)
-                    .offset(x: offset)
-                
-                TabView(selection: $activeTab) {
-                    Tab(TabState.home.rawValue, systemImage: TabState.home.symbolImage, value: .home) {
-                        Text("home")
-                    }
+                    .offset(x: isLandscape ? 0 : -sideBarWidth)
+                    .offset(x: isLandscape ? 0 : offset)
                     
-                    Tab(
-                        TabState.search.rawValue,
-                        systemImage: TabState.search.symbolImage,
-                        value: .search
-                    ) {
-                        Text("Search")
-                    }
-                    
-                    Tab(
-                        TabState.notifications.rawValue,
-                        systemImage: TabState.notifications.symbolImage,
-                        value: .notifications
-                    ) {
-                        Text("Notifications")
-                    }
-                    
-                    Tab(
-                        TabState.profile.rawValue,
-                        systemImage: TabState.profile.symbolImage,
-                        value: .profile
-                    ) {
-                        Text("Profile")
-                    }
-                }
-                .overlay {
-                    Rectangle()
-                        .fill(.black.opacity(0.25))
-                        .ignoresSafeArea()
-                        .opacity(progress)
-                }
-                .offset(x: offset)
-            }
-            .gesture(
-                CustomGesture { gesture in
-                    let state = gesture.state
-                    let translation = gesture.translation(
-                        in: gesture.view
-                    ).x + lastDragOffset
-                    
-                    let velocity = gesture.velocity(in: gesture.view).x / 3
-                    
-                    if state == .began || state == .changed {
-                        /// onChanged
-                        offset = max(min(translation, sideBarWidth), 0)
-                        
-                        /// storing drag progress for fading tab view when dragging effect
-                        progress = max(min(offset / sideBarWidth, 1), 0)
-                    } else {
-                        /// onEnded
-                        withAnimation(.snappy(duration: 0.25, extraBounce: 0)) {
-                            if (velocity + offset) > (sideBarWidth * 0.5) {
-                                /// Expand fully
-                                offset = sideBarWidth
-                                progress = 1
-                            } else { /// reset value
-                                offset = 0
-                                progress = 0
-                            }
+                    TabView(selection: $activeTab) {
+                        Tab(TabState.home.rawValue, systemImage: TabState.home.symbolImage, value: .home) {
+                            Text("home")
                         }
                         
-                        /// Saving last drag offset
-                        lastDragOffset = offset
+                        Tab(
+                            TabState.search.rawValue,
+                            systemImage: TabState.search.symbolImage,
+                            value: .search
+                        ) {
+                            Text("Search")
+                        }
+                        
+                        Tab(
+                            TabState.notifications.rawValue,
+                            systemImage: TabState.notifications.symbolImage,
+                            value: .notifications
+                        ) {
+                            Text("Notifications")
+                        }
+                        
+                        Tab(
+                            TabState.profile.rawValue,
+                            systemImage: TabState.profile.symbolImage,
+                            value: .profile
+                        ) {
+                            Text("Profile")
+                        }
                     }
+                    .tabViewStyle(.tabBarOnly) /// do not convert to side bar at iPadOS
+                    .overlay {
+                        Rectangle()
+                            .fill(.black.opacity(0.25))
+                            .ignoresSafeArea()
+                            .opacity(isLandscape ? 0 : progress)
+                    }
+                    .offset(x: isLandscape ? 0 : offset)
                 }
-            )
-//            /// iOS 18 gesture API ignore view with buttons, tap gesture and long press gesture
-//            /// highPriorityGesture() or SimultaneousGesture() does not have this issue but do not work with scroll view
-//            /// UI\ikit UiPanGesture does not have the above issues
-//            .gesture(DragGesture()
-//                .onChanged({ value in
-//                    let translation = value.translation.width + lastDragOffset
-//                    offset = max(min(translation, sideBarWidth), 0)
-//                    
-//                    /// storing drag progress for fading tab view when dragging effect
-//                    progress = max(min(offset / sideBarWidth, 1), 0)
-//                }).onEnded({ value in
-//                    let velocity = value.translation.width / 3
-//                    
-//                    withAnimation(.snappy(duration: 0.25, extraBounce: 0)) {
-//                        if (velocity + offset) > (sideBarWidth * 0.5) {
-//                            /// Expand fully
-//                            offset = sideBarWidth
-//                            progress = 1
-//                        } else { /// reset value
-//                            offset = 0
-//                            progress = 0
-//                        }
-//                    }
-//                    
-//                    /// Saving last drag offset
-//                    lastDragOffset = offset
-//                }))
+                .gesture(
+                    CustomGesture(isEnabled: !isLandscape) { gesture in
+                        /// UIGestureRepresentable doesn't update once the gesture has been created. Thus, isEnabled will have no effect.
+                        ///  Thus need to enable it manually
+                        if panGesture == nil { panGesture = gesture }
+
+                        let state = gesture.state
+                        let translation = gesture.translation(
+                            in: gesture.view
+                        ).x + lastDragOffset
+                        
+                        let velocity = gesture.velocity(in: gesture.view).x / 3
+                        
+                        if state == .began || state == .changed {
+                            /// onChanged
+                            offset = max(min(translation, sideBarWidth), 0)
+                            
+                            /// storing drag progress for fading tab view when dragging effect
+                            progress = max(min(offset / sideBarWidth, 1), 0)
+                        } else {
+                            /// onEnded
+                            withAnimation(.snappy(duration: 0.25, extraBounce: 0)) {
+                                if (velocity + offset) > (sideBarWidth * 0.5) {
+                                    /// Expand fully
+                                    offset = sideBarWidth
+                                    progress = 1
+                                } else { /// reset value
+                                    offset = 0
+                                    progress = 0
+                                }
+                            }
+                            
+                            /// Saving last drag offset
+                            lastDragOffset = offset
+                        }
+                    }
+                )
+                .onChange(of: isLandscape) { oldValue, newValue in
+                   panGesture?.isEnabled = !newValue
+                }
+                .navigationDestination(for: String.self) { value in
+                    Text("\(value) view")
+                        .navigationTitle(value)
+                }
+    //            /// iOS 18 gesture API ignore view with buttons, tap gesture and long press gesture
+    //            /// highPriorityGesture() or SimultaneousGesture() does not have this issue but do not work with scroll view
+    //            /// UI\ikit UiPanGesture does not have the above issues
+    //            .gesture(DragGesture()
+    //                .onChanged({ value in
+    //                    let translation = value.translation.width + lastDragOffset
+    //                    offset = max(min(translation, sideBarWidth), 0)
+    //
+    //                    /// storing drag progress for fading tab view when dragging effect
+    //                    progress = max(min(offset / sideBarWidth, 1), 0)
+    //                }).onEnded({ value in
+    //                    let velocity = value.translation.width / 3
+    //
+    //                    withAnimation(.snappy(duration: 0.25, extraBounce: 0)) {
+    //                        if (velocity + offset) > (sideBarWidth * 0.5) {
+    //                            /// Expand fully
+    //                            offset = sideBarWidth
+    //                            progress = 1
+    //                        } else { /// reset value
+    //                            offset = 0
+    //                            progress = 0
+    //                        }
+    //                    }
+    //
+    //                    /// Saving last drag offset
+    //                    lastDragOffset = offset
+    //                }))
+            }
+        }
+    }
+    
+    func toggleSideBar() {
+        withAnimation(.snappy(duration: 0.25, extraBounce: 0)) {
+            progress = 0
+            offset = 0
+            lastDragOffset = 0
         }
     }
 }
@@ -146,6 +174,7 @@ struct AdaptiveLayoutView: View {
 }
 
 fileprivate struct CustomGesture:UIGestureRecognizerRepresentable {
+    var isEnabled: Bool
     var handle: (UIPanGestureRecognizer) -> ()
     func makeUIGestureRecognizer(context: Context) -> UIPanGestureRecognizer {
         let gesture = UIPanGestureRecognizer()
@@ -156,6 +185,7 @@ fileprivate struct CustomGesture:UIGestureRecognizerRepresentable {
         _ recognizer: UIPanGestureRecognizer,
         context: Context
     ) {
+        recognizer.isEnabled = isEnabled
     }
     
     func handleUIGestureRecognizerAction(
@@ -163,5 +193,20 @@ fileprivate struct CustomGesture:UIGestureRecognizerRepresentable {
         context: Context
     ) {
         handle(recognizer)
+    }
+}
+
+struct AdaptiveView<Content: View>: View {
+    var showsSideBarOniPadPortrait: Bool = true
+    @ViewBuilder var content: (CGSize, Bool) -> Content
+    @Environment(\.horizontalSizeClass) private var hClass
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+            let isLandscape = size.width > size.height || (
+                hClass == .regular && showsSideBarOniPadPortrait
+            )
+            content(size, isLandscape)
+        }
     }
 }
--- View/SideBarView.swift
+++ View/SideBarView.swift
@@ -5,57 +5,77 @@
 import SwiftUI
 
 struct SideBarView: View {
+    @Binding var path: NavigationPath
+    var toggleSideBar: () -> ()
     var body: some View {
-        ScrollView(.vertical) {
-            VStack(alignment: .leading, spacing: 6) {
-                Image(.fox)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 60, height: 60)
-                    .clipShape(.circle)
-                
-                Text("Mr. Fox")
-                    .font(.callout)
-                    .fontWeight(.semibold)
-                
-                Text("@FoxFarm")
-                    .font(.caption2)
-                    .foregroundStyle(.gray)
-                
-                HStack(spacing: 4) {
-                    Text("3.1K")
+        GeometryReader {
+            let safeArea = $0.safeAreaInsets
+            let isSidesHavingValues = safeArea.leading != 0 || safeArea.trailing != 0
+            
+            ScrollView(.vertical) {
+                VStack(alignment: .leading, spacing: 6) {
+                    Image(.fox)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 60, height: 60)
+                        .clipShape(.circle)
+                    
+                    Text("Mr. Fox")
+                        .font(.callout)
                         .fontWeight(.semibold)
                     
-                    Text("Following")
-                        .font(.caption)
+                    Text("@FoxFarm")
+                        .font(.caption2)
                         .foregroundStyle(.gray)
                     
-                    Text("1.8M")
-                        .fontWeight(.semibold)
-                        .padding(.leading, 5)
+                    HStack(spacing: 4) {
+                        Text("3.1K")
+                            .fontWeight(.semibold)
+                        
+                        Text("Following")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                        
+                        Text("1.8M")
+                            .fontWeight(.semibold)
+                            .padding(.leading, 5)
+                        
+                        Text("Followers")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }
+                    .font(.system(size: 14))
+                    .lineLimit(1)
+                    .padding(.top, 5)
                     
-                    Text("Followers")
-                        .font(.caption)
-                        .foregroundStyle(.gray)
-                }
-                .font(.system(size: 14))
-                .lineLimit(1)
-                .padding(.top, 5)
-                
-                /// Side bar navigation items
-                VStack(alignment: .leading, spacing: 20) {
-                    ForEach(SideBarActions.allCases, id: \.rawValue) { action in
-                        SideBarActionButton(value: action) {
-                            
+                    /// Side bar navigation items
+                    VStack(alignment: .leading, spacing: 20) {
+                        ForEach(SideBarActions.allCases, id: \.rawValue) { action in
+                            SideBarActionButton(value: action) {
+                                toggleSideBar()
+                                path.append(action.rawValue) /// alt, pass the entire action (conforming to Hashable) and push view s based on it
+                            }
                         }
                     }
+                    .padding(25)
                 }
-                .padding(25)
+                .padding(.vertical, 15)
+                .padding(.horizontal, isSidesHavingValues ? 5 : 15)
             }
-            .padding(15)
+            .scrollClipDisabled()
+            .scrollIndicators(.hidden)
+            .background {
+                Rectangle()
+                    .fill(.background)
+                    .overlay(alignment: .trailing) {
+                        Rectangle()
+                            .fill(.gray.opacity(0.35))
+                            .frame(width: 1)
+                    }
+                    .ignoresSafeArea()
+            }
+            
         }
-        .scrollClipDisabled()
-        .scrollIndicators(.hidden)
     }
     
     @ViewBuilder
@@ -100,5 +120,6 @@ enum SideBarActions: String, CaseIterable {
 }
 
 #Preview {
-    SideBarView()
+    @Previewable @State var navigationPath: NavigationPath = .init()
+    SideBarView(path: $navigationPath, toggleSideBar: {})
 }
