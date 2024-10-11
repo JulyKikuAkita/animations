e958f806b66710b759cd28d230ef82ad007d2c0c
IFang Lee <b92701105@gmail.com>
2024-10-10 22:31:35 -0700

    adding ios18 ParallaxCarousel18View

--- View/Carousel/ParallelXCardView.swift
+++ View/Carousel/ParallelXCardView.swift
@@ -43,58 +43,96 @@ struct TravelCardView: View {
                     .padding(.top, 10)
                 
                 /// Parallax Carousel
-                GeometryReader(content: { geometry in
-                    let size = geometry.size
-                    
-                    ScrollView(.horizontal) {
-                        HStack(spacing: 10) {
-                            ForEach(firstSetCards) { card in
-                                GeometryReader(content: { proxy in
-                                    let cardSize = proxy.size
-                                    /// Simple Parallax effect (1)
-                                    let minX = proxy.frame(in: .scrollView).minX - 30.0
-                                    /// Simple Parallax effect (2)
-//                                    let minX = min((proxy.frame(in: .scrollView).minX - 30.0), proxy.size.width * 1.4)
-                                    
-                                    Image(card.image)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        //.scaleEffect(1.25)
-                                        .offset(x: -minX)
-                                        .frame(width: proxy.size.width * 2.5) // or use scaling -> .scaleEffect(1.25)
-                                        .frame(width: cardSize.width, height: cardSize.height)
-                                        .overlay {
-                                            OverlayView(card)
-                                            //  Text("\(minX)")
-                                            //  .font(.largeTitle)
-                                            //   .foregroundStyle(.white)
-                                        }
-                                        .clipShape(.rect(cornerRadius: 15))
-                                        .shadow(color: .black.opacity(0.25), radius: 8, x: 5, y: 10)
-                                })
-                                .frame(width: size.width - 60, // size of padding 30
-                                       height: size.height - 50)
-                                /// Scroll Animation
-                                .scrollTransition(.interactive, axis: .horizontal) { view, phase in
-                                    view
-                                        .scaleEffect(phase.isIdentity ? 1 : 0.95)
-                                }
-                            }
-                        }
-                        .padding(.horizontal, 30)
-                        .scrollTargetLayout() // iOS 17 new scroll api
-                        .frame(height: size.height, alignment: .top)
-            
-                    }
-                    .scrollTargetBehavior(.viewAligned) // iOS 17 new scroll api
-                    .scrollIndicators(.hidden)
-                })
+                GeometryReader { geometry in
+                    let minX = geometry.frame(in: .scrollView).minX - 30.0
+
+                    ParallaxCarousel18View(size: geometry.size)
+                        .offset(x: -minX)
+//                    ParallaxCarousel17View(size: geometry.size)
+                }
                 .frame(height: 500)
                 .padding(.horizontal, -15)
                 .padding(.top, 10)
             }
             .padding(15)
         }
+        .scrollIndicators(.hidden)
+    }
+    
+    @ViewBuilder
+    func ParallaxCarousel18View(size: CGSize) -> some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
+                ForEach(firstSetCards) { card in
+
+                    Image(card.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width + 80) // 80 is the offset value
+                        .scrollTransition(.interactive, axis: .horizontal) { content, phase in
+                            content
+                                .offset(x: phase == .identity ? 0 : -phase.value * 80)
+                        }
+                        .frame(width: size.width, height: size.height)
+                        .overlay {
+                            OverlayView(card)
+                        }
+                        .clipShape(.rect(cornerRadius: 25))
+                        .shadow(color: .black.opacity(0.25), radius: 8, x: 5, y: 10)
+                }
+            }
+            .padding(.horizontal, 30)
+            .scrollTargetLayout()
+            .frame(height: size.height, alignment: .top)
+        }
+        .scrollTargetBehavior(.viewAligned(limitBehavior: .always))
+        .safeAreaPadding(.horizontal, 15)
+        .scrollIndicators(.hidden)
+    }
+    
+    @ViewBuilder
+    func ParallaxCarousel17View(size: CGSize) -> some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 10) {
+                ForEach(firstSetCards) { card in
+                    GeometryReader(content: { proxy in
+                        let cardSize = proxy.size
+                        /// Simple Parallax effect (1)
+                        let minX = proxy.frame(in: .scrollView).minX - 30.0
+                        /// Simple Parallax effect (2)
+//                                    let minX = min((proxy.frame(in: .scrollView).minX - 30.0), proxy.size.width * 1.4)
+                        
+                        Image(card.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            //.scaleEffect(1.25)
+                            .offset(x: -minX)
+                            .frame(width: proxy.size.width * 2.5) // or use scaling -> .scaleEffect(1.25)
+                            .frame(width: cardSize.width, height: cardSize.height)
+                            .overlay {
+                                OverlayView(card)
+                                //  Text("\(minX)")
+                                //  .font(.largeTitle)
+                                //   .foregroundStyle(.white)
+                            }
+                            .clipShape(.rect(cornerRadius: 15))
+                            .shadow(color: .black.opacity(0.25), radius: 8, x: 5, y: 10)
+                    })
+                    .frame(width: size.width - 60, // size of padding 30
+                           height: size.height - 50)
+                    /// Scroll Animation
+                    .scrollTransition(.interactive, axis: .horizontal) { view, phase in
+                        view
+                            .scaleEffect(phase.isIdentity ? 1 : 0.95)
+                    }
+                }
+            }
+            .padding(.horizontal, 30)
+            .scrollTargetLayout() // iOS 17 new scroll api
+            .frame(height: size.height, alignment: .top)
+
+        }
+        .scrollTargetBehavior(.viewAligned) // iOS 17 new scroll api
         .scrollIndicators(.hidden)
     }
     
@@ -128,5 +166,5 @@ struct TravelCardView: View {
 }
 
 #Preview {
-    ContentView()
+    ParallelXCardView()
 }
