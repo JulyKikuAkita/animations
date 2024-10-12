3b2fd06d342d8fbe790630f34f559f5c30cd35d7
IFang Lee <b92701105@gmail.com>
2024-10-12 16:28:01 -0700

    add stack card carousel

--- View/Carousel/CardCarouselWithScrollTransitionsAPI.swift
+++ View/Carousel/CardCarouselWithScrollTransitionsAPI.swift
@@ -106,4 +106,49 @@ struct CardCarouselWithScrollTransitionsAPIView: View {
         
 #Preview {
     CardCarouselWithScrollTransitionsAPIView()
+    StackCardCarouselView()
+}
+
+/// Stack card carousel
+struct StackCardCarouselView: View {
+    var body: some View {
+        NavigationStack {
+            GeometryReader {
+                let size = $0.size
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 10) {
+                        ForEach(firstSetCards) { card in
+                            let index = Double(firstSetCards.firstIndex(where: { $0.id == card.id }) ?? 0)
+                            
+                            GeometryReader {
+                                let minX = $0.frame(in: .scrollView(axis: .horizontal)).minX
+                                
+                                Image(card.image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 220, height: size.height)
+                                    .clipShape(.rect(cornerRadius: 25))
+                                    .scrollTransition(.interactive, axis: .horizontal) { content, phase in
+                                        content
+                                            .blur(radius: phase == .identity ? 0 : 2, opaque: false)
+                                            .scaleEffect(phase == .identity ? 1: 0.9, anchor: .bottom)
+                                            .offset(y: phase == .identity ? 0 : -10)
+                                            .rotationEffect(.init(degrees: phase == .identity ? 0 : phase.value * 5), anchor: .bottomTrailing)
+                                            .offset(x: minX < 0 ? minX / 2 : -minX)
+                                    }
+                            }
+                            .frame(width: 220)
+                            .zIndex(-index)
+                        }
+                    }
+                    .scrollTargetLayout()
+                }
+                .scrollClipDisabled()
+                .scrollTargetBehavior(.viewAligned(limitBehavior: .always))
+                .safeAreaPadding(.horizontal, (size.width - 220) / 2)
+            }
+        }
+        .safeAreaPadding(.horizontal, 15)
+        .frame(height: 330)
+    }
 }
