3aca9b72af7927f119cb20dad75f412e56c15212
IFang Lee <b92701105@gmail.com>
2025-05-17 17:06:21 -0700

    add ResizableHeaderScrollViewDemo with ios18 api

--- Model/OSInfo.swift
+++ Model/OSInfo.swift
@@ -2,12 +2,14 @@
 //  OSInfo.swift
 //  animation
 
-enum OSInfo: String, CaseIterable {
+enum OSInfo: String, CaseIterable, Hashable {
     case iOS
     case appleWatch = "watchOS"
     case ipad = "iPadOS"
     case macbook = "macOS"
     case visionOS
+    case homePod
+    case airPod
 
     var symbolImage: String {
         switch self {
@@ -16,6 +18,8 @@ enum OSInfo: String, CaseIterable {
         case .ipad: "ipad"
         case .macbook: "macbook"
         case .visionOS: "vision.pro"
+        case .homePod: "homepod"
+        case .airPod: "airpods"
         }
     }
 }
