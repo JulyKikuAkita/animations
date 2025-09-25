9672fb0f62c67eb1aaa13dbde928fa606c4712ee
JulyKikuAkita <b92701105@gmail.com>
2025-09-25 16:54:44 -0700

    add pdfmaker app

---
+++ PDFMaker/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  PDFMaker
+//
+//  Created on 9/25/25.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+//        PDFRawView()
+        PDFStatementView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDFMaker/PDFMakerApp.swift
@@ -0,0 +1,16 @@
+//
+//  PDFMakerApp.swift
+//  PDFMaker
+//
+//  Created on 9/25/25.
+
+import SwiftUI
+
+@main
+struct PDFMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
