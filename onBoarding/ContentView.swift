0e09d47dd1dba8f86cd2bc14cd5bee66a9f1d978
JulyKikuAkita <banananoonoodles@gmail.com>
2025-11-15 14:30:44 -0800

    add custom LaunchScreenIOS26DemoApp (#122)

--- onBoarding/ContentView.swift
+++ onBoarding/ContentView.swift
@@ -8,26 +8,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        OneTimeOnBoarding(appStorageID: "Home_Turorial") {
-            VStack {
-                VStack {
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                    Text("Hello, world!")
-                }
-                .padding()
-                .onBoarding(1) {
-                    dummyTextView()
-                }
-
-                Button("Donwload") {}
-                    .padding(15)
-                    .onBoarding(2) {
-                        dummyTextView()
-                    }
-            }
-        } beginOnboarding: {} onBoardingFinished: {}
+        dummyTextView()
     }
 
     func dummyTextView() -> some View {
